a3ec379f90bddfd4e1ee2770d8a84361d9fc10ef
Sam Chen <sam8211112222@gmail.com>
2023-01-29 11:18:36 +0800

    add state as search term

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,22 @@
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
+import {ActionCreator} from '../state'
+
 const RepositoriesList: React.FC = () => {
-  return (
-    <div>
-      <form>
-        <input />
-        <button>Search</button>
-      </form>
-    </div>
-  );
+    const [term, setTerm] = useState<string>('');
+    const dispatch = useDispatch();
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault()
+        dispatch(ActionCreator.searchRepositories(term) as any) ;
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={event => setTerm(event.target.value)}/>
+                <button>Search</button>
+            </form>
+        </div>
+    );
 };
 
 export default RepositoriesList;
